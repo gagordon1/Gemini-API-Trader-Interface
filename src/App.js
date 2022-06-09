3634406118e82a273c0f6cb55f009144e10490bb
Garrett Gordon <gagordon@mit.edu>
2022-06-09 13:41:42 -0400

    react interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div >
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      amountBought : 0,
+      amountSold : 0,
+      paidForBuys : 0,
+      earnedFromSells : 0
+    }
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(()=> this.getItems(), 4000);
+  }
+
+  componentWillUnmount() {
+    this.timer = clearInterval(this.timer); // here...
+    this.timer = null
+  }
+
+  getItems() {
+      fetch("http://localhost:8080/stats")
+        .then(result => result.json())
+        .then(result => this.setState({
+            amountBought : result["amountBought"],
+            amountSold : result["amountSold"],
+            paidForBuys : result["totalPaidForBuys"],
+            earnedFromSells : result["totalEarnedFromSells"]
+        })
+      );
+  }
+  render() {
+
+    return (
+      <div>
+        <p> Amount Bought: {this.state.amountBought}</p>
+        <p> Amount Sold: {this.state.amountSold}</p>
+        <p> Total Paid for Buys: ${this.state.paidForBuys}</p>
+        <p> Total Earned from Sells: ${this.state.earnedFromSells}</p>
+      </div>
+    )
+  }
 }
-
-export default App;
